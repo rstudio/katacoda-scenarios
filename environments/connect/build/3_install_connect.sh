b6ad72d26ae10ef18f9dca11ab9ea6d98a4728a5
Ferit Albukrek <falbukrek@gmail.com>
2019-12-20 12:44:43 -0500

    Using the latest installer

--- environments/connect/build/3_install_connect.sh
+++ environments/connect/build/3_install_connect.sh
@@ -1,7 +1,8 @@
 
-curl -LO https://rstudio-connect.s3.amazonaws.com/rsc-installer.sh
+wget -q https://s3.amazonaws.com/rstudio-connect/rstudio-connect_1.8.1-5622_amd64.deb
 
 export RUN_UNATTENDED=1
-export RUN_PRIVILEGED=0
+export RUN_PRIVILEGED=1
+export RSTUDIO_INSTALL_NO_LICENSE_INITIALIZATION=1
 
-bash ./rsc-installer.sh install
+apt install -y rstudio-connect_1.8.1-5622_amd64.deb
