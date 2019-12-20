5c6989375dc7d30ded050373c99a1d6e3b3b6488
Ferit Albukrek <falbukrek@gmail.com>
2019-12-20 12:48:08 -0500

    Fixed path to deb file.

--- environments/connect/build/3_install_connect.sh
+++ environments/connect/build/3_install_connect.sh
@@ -5,4 +5,4 @@ export RUN_UNATTENDED=1
 export RUN_PRIVILEGED=1
 export RSTUDIO_INSTALL_NO_LICENSE_INITIALIZATION=1
 
-apt install -y rstudio-connect_1.8.1-5622_amd64.deb
+apt install -y ./rstudio-connect_1.8.1-5622_amd64.deb
